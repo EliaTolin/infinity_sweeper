1a213152280203c6586bb215625f492cfd250114
Elia Tolin <eliatolin@gmail.com>
2021-12-11 18:59:37 +0100

    Add Safe Area

--- lib/screens/game_page.dart
+++ lib/screens/game_page.dart
@@ -30,39 +30,41 @@ class _GamePageState extends State<GamePage> {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: StyleConstant.mainColor,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          InfoBar(size.height),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(StyleConstant.kPaddingComponent),
-              child: ClayContainer(
-                borderRadius: 5,
-                curveType: CurveType.none,
-                surfaceColor: StyleConstant.mainColor,
-                parentColor: StyleConstant.mainColor,
-                color: StyleConstant.mainColor,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5.0),
-                    ),
-                    child: LayoutBuilder(
-                      builder:
-                          (BuildContext context, BoxConstraints constraints) =>
-                              Center(
-                        child: Consumer<GameModel>(
-                          builder: (context, gameModel, child) {
-                            //throw if cellGrid is null
-                            CellGrid? cellGrid =
-                                Provider.of<GameModel>(context, listen: false)
-                                    .cellGrid;
-                            if (cellGrid!.grid.isEmpty) return Container();
-                            return MineSweeperCore(cellGrid.grid,
-                                cellGrid.sizeGrid, cellGrid.numMines);
-                          },
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            InfoBar(size.height),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(StyleConstant.kPaddingComponent),
+                child: ClayContainer(
+                  borderRadius: 5,
+                  curveType: CurveType.none,
+                  surfaceColor: StyleConstant.mainColor,
+                  parentColor: StyleConstant.mainColor,
+                  color: StyleConstant.mainColor,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                      child: LayoutBuilder(
+                        builder: (BuildContext context,
+                                BoxConstraints constraints) =>
+                            Center(
+                          child: Consumer<GameModel>(
+                            builder: (context, gameModel, child) {
+                              //throw if cellGrid is null
+                              CellGrid? cellGrid =
+                                  Provider.of<GameModel>(context, listen: false)
+                                      .cellGrid;
+                              if (cellGrid!.grid.isEmpty) return Container();
+                              return MineSweeperCore(cellGrid.grid,
+                                  cellGrid.sizeGrid, cellGrid.numMines);
+                            },
+                          ),
                         ),
                       ),
                     ),
@@ -70,8 +72,8 @@ class _GamePageState extends State<GamePage> {
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
