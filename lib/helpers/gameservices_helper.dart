00b408c282931e21f6bbeaef4d050a831c6be8b1
Elia Tolin <eliatolin@gmail.com>
2022-02-26 12:33:25 +0100

    fix gameservices

--- lib/helpers/gameservices_helper.dart
+++ lib/helpers/gameservices_helper.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/services.dart';
 import 'package:games_services/games_services.dart';
 import 'package:infinity_sweeper/constants/gameservices_constant.dart';
+import 'package:infinity_sweeper/helpers/logger_helper.dart';
 import 'package:infinity_sweeper/models/game/gamedifficulty_model.dart';
 
 class GamesServicesHelper {
@@ -18,13 +21,9 @@ class GamesServicesHelper {
   void loadGamesService() async {
     try {
       await GamesServices.signIn();
-<<<<<<< HEAD
     } on PlatformException catch (e) {
-      print(e.message);
+      LoggerHelper.print(e.message);
     }
-=======
-    } on Exception catch (_) {}
->>>>>>> 9f322af41c132f4a0dc8dca64c22a8556c46f511
   }
 
   void submitScore(int score, Difficulty difficulty) async {
